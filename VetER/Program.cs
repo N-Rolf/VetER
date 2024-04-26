e38d5767748467b25ac259566eef66e40390178c
N-Rolf <neilrolf24@gmail.com>
2024-04-26 10:42:42 -0500

    Add project files.

---
+++ VetER/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetER
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    enum TriageLevel
+    {
+        Red = 1,
+        Yellow = 2,
+        Green = 3
+    }
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Symptoms { get; set; }
+        public string Medications { get; set; }
+        public int TraiageLevel { get; set; }
+    }
+
+    public class Owner
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Animals { get; set; }
+    }
+}
